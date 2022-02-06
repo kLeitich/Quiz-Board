0a1c3d61c862ed053f1c0ff5e6be866ffd61ab5d
kLeitich <kevleitich@gmail.com>
2022-02-06 12:46:32 +0300

    This commit add js code that get files form the the two form in HTML

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-// var correctAnswer = {
-//     q1: "100%"
-//     q2: "Markup Language"
-//     q3:"/* a comment */"
-//     q4: "p {color: red;}"
-//     q5: "a {text-decoration:none;}"
-// }
+var correctAnswer = {
+    q1: "100%"
+    q2: "Markup Language"
+    q3:"/* a comment */"
+    q4: "p {color: red;}"
+    q5: "a {text-decoration:none;}"
+}
 
 
 //get data from userinfo
-let userInfo = document.getElementById("userInfo")
+let userInfo = document.getElementById("#userInfo")
 
+
+//get data from questionform
+
+let questionForm = document.getElementById("#questionform")
 //move to the next slide
 
-userInfo.addEventListener.btn("submit") {
-    console.log("userInfo")
-};
