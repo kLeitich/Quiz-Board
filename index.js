b0372419a175b4b7a9c86d180e9b750b1ae60d95
kLeitich <kevleitich@gmail.com>
2022-02-07 00:08:00 +0300

    This commit adds my second trial in trying to move between slide, still with bug and not succeful

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-var correctAnswer = {
-    q1: "100%",
-    q2: "Markup Language",
-    q3:"/* a comment */",
-    q4: "p {color: red;}",
-    q5: "a {text-decoration:none;}"
-}
+// var correctAnswer = {
+//     q1: "100%",
+//     q2: "Markup Language",
+//     q3:"/* a comment */",
+//     q4: "p {color: red;}",
+//     q5: "a {text-decoration:none;}"
+// }
 
 
 //get data from userinfo
 let userInfo = document.getElementById("#userInfo")
 
 
-//get data from questionform
-
-let questionForm = document.getElementById("#questionform")
 //move to the next slide
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+// userInfo.addEventListener("submit", function (event){
+// event
+// };
+
+
+// //get data from questionform
+
+// let questionForm = document.getElementById("#questionform")
+
 
