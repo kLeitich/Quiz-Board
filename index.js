84d662a8859f4fa7cd5b7222b607c29b5138aee4
kLeitich <kevleitich@gmail.com>
2022-02-06 12:43:22 +0300

    this commit adds my first trail on js and has alot of bugs

---
+++ index.js
@@ -0,0 +1,17 @@
+// var correctAnswer = {
+//     q1: "100%"
+//     q2: "Markup Language"
+//     q3:"/* a comment */"
+//     q4: "p {color: red;}"
+//     q5: "a {text-decoration:none;}"
+// }
+
+
+//get data from userinfo
+let userInfo = document.getElementById("userInfo")
+
+//move to the next slide
+
+userInfo.addEventListener.btn("submit") {
+    console.log("userInfo")
+};
